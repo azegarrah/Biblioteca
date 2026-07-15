7a54c225d0817c4fa993ff89e8a0f5837335ef5a
WilliamJosueMP <elhermanowilliam@gmail.com>
2026-07-15 08:03:50 -0500

    Archivo agregado con comentarios de Lector.cpp

--- Lector.cpp
+++ Lector.cpp
@@ -3,31 +3,40 @@
 using namespace std;
 
 void agregarLector(Lector Lec[], int& n) {
+        cout << "Ingrese el codigo del lector: ";
         cin >> lec[n].codigo;
-        cin >> lec[n].nombre;
-        cin >> lec[n].correo;
-        cin >> lec[n].facultad;
+        cin.ignore();
+
+        cout << "Ingrese el nombre del lector: ";
+        getline(cin, lec[n].nombre);
+        
+        cout << "Ingrese el correo del lector: ";
+        getline(cin, lec[n].correo);
+        
+        cout << "Ingrese la facultad del lector: ";
+        getline(cin, lec[n].facultad);
         n++;
 }
 
 void mostrarLectores(Lector lec[], int n) {
     for (int i = 0; i < n; i++) {
-        cout << lec[i].codigo << " ";
-        cout << lec[i].nombre << " ";
-        cout << lec[i].correo << " ";
-        cout << lec[i].facultad << endl;
+        cout <<"Codigo: " << lec[i].codigo << endl;
+        cout << "Nombre: " << lec[i].nombre << endl;
+        cout << "Correo: " << lec[i].correo << endl;
+        cout << "Facultad: " << lec[i].facultad << endl;
+        cout << endl;
     }
 }
 
 void buscarLector(Lector lec[], int n) {
     int codigo;
+    cout << "Ingrese el codigo del lector a buscar: ";
     cin >> codigo;
-
     for (int i = 0; i < n; i++) {
         if (lec[i].codigo == codigo) {
-            cout << lec[i].codigo << " ";
-            cout << lec[i].nombre << " ";
-            cout << lec[i].correo << " ";
+            cout << "Codigo: " << lec[i].codigo << endl;
+            cout << "Nombre: " << lec[i].nombre << endl;
+            cout << "Correo: " << lec[i].correo << endl;
             cout << lec[i].facultad << endl;
             return;
         }
@@ -37,13 +46,17 @@ void buscarLector(Lector lec[], int n) {
 
 void editarLector(Lector lec[], int n) {
     int codigo;
+    cout << "Ingrese el codigo del lector a editar: ";
     cin >> codigo;
-
+    cin.ignore();
     for (int i = 0; i < n; i++) {
         if (lec[i].codigo == codigo) {
-            cin >> lec[i].nombre;
-            cin >> lec[i].correo;
-            cin >> lec[i].facultad;
+            cout << "Ingrese el nuevo nombre del lector: ";
+            getline(cin, lec[i].nombre);
+            cout << "Ingrese el nuevo correo del lector: ";
+            getline(cin, lec[i].correo);
+            cout << "Ingrese la nueva facultad del lector: ";
+            getline(cin, lec[i].facultad);
             return;
         }
     }
@@ -51,14 +64,16 @@ void editarLector(Lector lec[], int n) {
 
 void eliminarLector(Lector lec[], int& n) {
     int codigo;
+    cout << "Ingrese el codigo del lector a eliminar: ";
     cin >> codigo;
-
     for (int i = 0; i < n; i++) {
         if (lec[i].codigo == codigo) {
             for (int j = i; j < n - 1; j++) {
                 lec[j] = lec[j + 1];
             }
             n--;
+            return;
+            cout << "Lector eliminado." << endl;
             return;
         }
     }
