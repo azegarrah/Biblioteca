02ccfe3d014a1089ae0f669b5afd9f2d8670b8a5
WilliamJosueMP <elhermanowilliam@gmail.com>
2026-07-15 05:11:16 -0500

    Cear archivo Materia.h

--- Material.h
+++ Material.h
@@ -3,3 +3,20 @@
 
 #include <iostream>
 using namespace std;
+
+struct Material {
+    int codigo;
+    string titulo;
+    string autor;
+    int anioPublicacion;
+    bool prestado;
+};
+
+void agregarMaterial(Material mat[100], int& n);
+void mostrarMateriales(Material mat[100], int n);
+void buscarMaterial(Material mat[100], int n);
+void editarMaterial(Material mat[100], int n);
+void eliminarMaterial(Material mat[100], int& n);
+void ordenarMaterial(Material mat[100], int n);
+
+#endif
