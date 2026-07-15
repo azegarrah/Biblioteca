8f1e1396ed08de30dfa9a2d43e449320e9fdd764
WilliamJosueMP <elhermanowilliam@gmail.com>
2026-07-14 20:32:31 -0500

    Crear estructura del archivo Lector.h

--- Lector.h
+++ Lector.h
@@ -3,3 +3,21 @@
 
 #include <iostream>
 using namespace std;
+
+struct Lector {
+    int codigo;
+    string nombre;
+    string correo;
+    string facultad;
+};
+
+extern Lector lectores[100];
+extern int cantLectores;
+
+void agregarLector();
+void mostrarLectores();
+void buscarLector();
+void editarLector();
+void eliminarLector();
+
+#endif // LECTRO_H
